ad93a947302e38094589de0270a22c9acf91f0b3
Kell <gilsonvigilante@outlook.com>
2026-07-12 23:48:43 -0300

    inicio do projeto

---
+++ src/main/java/dev/kellyson/alexandriabank/AlexandriaBankApplication.java
@@ -0,0 +1,13 @@
+package dev.kellyson.alexandriabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandriaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandriaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kellyson/alexandriabank/AlexandriaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kellyson.alexandriabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandriaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
