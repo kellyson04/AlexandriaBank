880ed927831721c6385913ee10ac6da6ec3a555b
Kell <gilsonvigilante@outlook.com>
2026-07-21 16:59:54 -0300

    feat: expõe endpoint para cadastro de clientes

---
+++ src/main/java/dev/kellyson/alexandriabank/cliente/ClienteController.java
@@ -0,0 +1,25 @@
+package dev.kellyson.alexandriabank.cliente;
+
+import dev.kellyson.alexandriabank.cliente.dto.ClienteCriadoResponse;
+import dev.kellyson.alexandriabank.cliente.dto.CriarClienteRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<ClienteCriadoResponse> criarCliente(@RequestBody @Valid CriarClienteRequest clienteRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.criarCliente(clienteRequest));
+    }
+}
