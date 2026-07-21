4b0eb1c8fbed2d35500856d4dcbc7a043bdeb678
Kell <gilsonvigilante@outlook.com>
2026-07-21 16:51:36 -0300

    feat: adiciona persistência e consultas de usuários

---
+++ src/main/java/dev/kellyson/alexandriabank/usuario/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package dev.kellyson.alexandriabank.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+}
