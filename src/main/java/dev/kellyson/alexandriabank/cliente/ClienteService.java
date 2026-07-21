b02c7e8cc0a22812e104c7f3206a9a70b1ea98b7
Kell <gilsonvigilante@outlook.com>
2026-07-21 16:59:54 -0300

    feat: implementa cadastro seguro de clientes

---
+++ src/main/java/dev/kellyson/alexandriabank/cliente/ClienteService.java
@@ -0,0 +1,50 @@
+package dev.kellyson.alexandriabank.cliente;
+
+import dev.kellyson.alexandriabank.cliente.dto.ClienteCriadoResponse;
+import dev.kellyson.alexandriabank.cliente.dto.CriarClienteRequest;
+import dev.kellyson.alexandriabank.conta.ContaService;
+import dev.kellyson.alexandriabank.usuario.PerfilUsuario;
+import dev.kellyson.alexandriabank.usuario.Usuario;
+import dev.kellyson.alexandriabank.usuario.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final ContaService contaService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public ClienteCriadoResponse criarCliente(CriarClienteRequest clienteRequest) {
+
+        String senhaCodificada = passwordEncoder.encode(clienteRequest.senha());
+
+        Usuario novoCliente = new Usuario(
+                clienteRequest.nome(),
+                clienteRequest.cpf(),
+                clienteRequest.email(),
+                senhaCodificada,
+                PerfilUsuario.CLIENTE
+        );
+
+        if (usuarioRepository.existsByCpf(novoCliente.getCpf())) {
+            throw new IllegalArgumentException("CPF já cadastrado");
+        }
+
+        if (usuarioRepository.existsByEmail(novoCliente.getEmail())) {
+            throw new IllegalArgumentException("Email já cadastrado");
+        }
+
+        usuarioRepository.save(novoCliente);
+
+        contaService.abrirConta(novoCliente);
+
+        return ClienteMapper.paraResposta(novoCliente);
+    }
+
+}
