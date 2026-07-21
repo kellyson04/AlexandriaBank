9bca0a56f90ce508848aeb17d188d68b3b7ae81b
Kell <gilsonvigilante@outlook.com>
2026-07-21 16:50:44 -0300

    feat: cria entidade de usuário integrada ao Spring Security

---
+++ src/main/java/dev/kellyson/alexandriabank/usuario/Usuario.java
@@ -0,0 +1,91 @@
+package dev.kellyson.alexandriabank.usuario;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario implements UserDetails {
+
+    public Usuario(String nome, String cpf, String email, String senha, PerfilUsuario perfil) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        this.perfil = perfil;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String nome;
+
+    @Column(nullable = false, updatable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false, unique = true, length = 180)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private PerfilUsuario perfil;
+
+    @Column(nullable = false, updatable = false)
+    private Instant criadoEm;
+
+    @PrePersist
+    void prePersist() {
+        this.criadoEm = Instant.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.perfil.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
