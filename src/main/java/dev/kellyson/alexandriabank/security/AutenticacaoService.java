b32bc9c8150ee5f4600b24aadce5b4ef804f9852
Kell <gilsonvigilante@outlook.com>
2026-07-21 17:00:52 -0300

    feat: integra carregamento de usuários ao Spring Security

---
+++ src/main/java/dev/kellyson/alexandriabank/security/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package dev.kellyson.alexandriabank.security;
+
+import dev.kellyson.alexandriabank.usuario.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario nao encontrado"));
+    }
+}
