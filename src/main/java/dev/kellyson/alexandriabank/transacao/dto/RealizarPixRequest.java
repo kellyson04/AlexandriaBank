3a4338789d79c4eda2fc0c7447d6d9f8930eba7e
Kell <gilsonvigilante@outlook.com>
2026-07-21 17:00:30 -0300

    feat: valida dados de entrada para realização de Pix

---
+++ src/main/java/dev/kellyson/alexandriabank/transacao/dto/RealizarPixRequest.java
@@ -0,0 +1,18 @@
+package dev.kellyson.alexandriabank.transacao.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record RealizarPixRequest(
+        @NotNull(message = "A conta de destino é obrigatória")
+        @Positive(message = "A conta de destino deve ser valida")
+        Long idContaDestino,
+
+        @NotNull(message = "O valor e obrigatório")
+        @DecimalMin(value = "0.01", message = "O valor deve ser maior que zero")
+        BigDecimal valor
+) {
+}
